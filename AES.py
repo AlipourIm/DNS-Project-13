cd75c79744834d188987135be3b7891f31b69917
AlipourIm <iman.alip2001@gmail.com>
2023-06-06 12:11:51 +0330

    Add AES logic

---
+++ AES.py
@@ -0,0 +1,38 @@
+import base64
+import os
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+import Resources
+
+
+def generate_symmetric_key(raw_key: str):
+    key = Resources.get_hash(raw_key)[:32]
+    return key
+
+
+def encrypt(message: str, key: str, iv):
+    tmp_message = message
+    extra = len(tmp_message) % 16
+    if extra > 0:
+        tmp_message = tmp_message + (' ' * (16 - extra))
+    cipher = Cipher(algorithms.AES(key.encode("ASCII")), modes.CBC(iv.encode("ASCII")))
+    encryptor = cipher.encryptor()
+    cipher_text = encryptor.update(tmp_message.encode("ASCII")) + encryptor.finalize()
+    return base64.b64encode(cipher_text).decode("ASCII")
+
+
+def decrypt(cipher_text: str, key: str, iv):
+    cipher = Cipher(algorithms.AES(key.encode("ASCII")), modes.CBC(iv.encode("ASCII")))
+    decipher = cipher.decryptor()
+    plain = decipher.update(base64.b64decode(cipher_text.encode("ASCII"))) + decipher.finalize()
+    return plain.decode("ASCII").strip()
+
+
+def test():
+    message = "1234567812345678sadf         "
+    iv = "0000000000000000"
+    print(iv)
+    key = generate_symmetric_key("1234")
+    cipher_text = encrypt(message, key, iv)
+    print(cipher_text)
+    plain_text = decrypt(cipher_text, key, iv)
+    print(plain_text)
